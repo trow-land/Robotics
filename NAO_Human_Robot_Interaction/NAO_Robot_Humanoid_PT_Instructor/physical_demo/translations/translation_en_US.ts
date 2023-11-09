a4a0855fe6b3b609f9c4a76643f05b7ee31bd4ab
Tom Rowland <75323342+trow-land@users.noreply.github.com>
2023-11-09 14:16:01 +0000

    Add files via upload

---
+++ NAO_Human_Robot_Interaction/NAO_Robot_Humanoid_PT_Instructor/physical_demo/translations/translation_en_US.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stand with your feet shoulder-width apart and extend your arms straight out to the sides, parallel to the ground.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stand with your feet shoulder-width apart and extend your arms straight out to the sides, parallel to the ground.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Engage your core and keep your chest up.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Engage your core and keep your chest up.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Without bending your elbows, twist your hands all the way forward as far as you can go.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Without bending your elbows, twist your hands all the way forward as far as you can go.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Pause for a moment, then twist your arms in the reverse direction as far as you can go. 
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Pause for a moment, then twist your arms in the reverse direction as far as you can go. 
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Repeat this motion and keep in time with me! 
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Repeat this motion and keep in time with me! 
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well Done!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well Done!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am going to explain how to do some arm circles. Listen to these instructions and then follow my lead.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am going to explain how to do some arm circles. Listen to these instructions and then follow my lead.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Circles/Animated Say (7)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stand with your feet shoulder-width apart and extend your arms straight out to the sides, parallel to the ground.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stand with your feet shoulder-width apart and extend your arms straight out to the sides, parallel to the ground.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Engage your core and keep your chest up.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Engage your core and keep your chest up.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Without bending your elbows, twist your hands all the way forward as far as you can go.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Without bending your elbows, twist your hands all the way forward as far as you can go.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Pause for a moment, then twist your arms in the reverse direction as far as you can go. 
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Pause for a moment, then twist your arms in the reverse direction as far as you can go. 
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Repeat this motion and keep in time with me! 
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Repeat this motion and keep in time with me! 
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well Done!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well Done!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arm Twists/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well done! Five repetitions done! Good job!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well done! Five repetitions done! Good job!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stand with your feet shoulder-width apart and hold your arms at your sides.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stand with your feet shoulder-width apart and hold your arms at your sides.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Engage your core and keep your chest up.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Engage your core and keep your chest up.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lift your arms out to the sides, keeping your arms straight and your palms facing down.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lift your arms out to the sides, keeping your arms straight and your palms facing down.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lift your arms up until they are at shoulder height, or slightly above.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lift your arms up until they are at shoulder height, or slightly above.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hold the position for a second, then slowly lower your arms back to your sides.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hold the position for a second, then slowly lower your arms back to your sides.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can do this exercise with or without light dumbells depending on how strong you feel.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You can do this exercise with or without light dumbells depending on how strong you feel.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat Raises/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say</name>
+        <message>
+            <source>Well done! Five repetitions done! Good job!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done! Five repetitions done! Good job!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (1)</name>
+        <message>
+            <source>Stand with your feet shoulder-width apart and hold your arms at your sides.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Stand with your feet shoulder-width apart and hold your arms at your sides.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (2)</name>
+        <message>
+            <source>Engage your core and keep your chest up.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Engage your core and keep your chest up.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (3)</name>
+        <message>
+            <source>Lift your arms out to the sides, keeping your arms straight and your palms facing down.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Lift your arms out to the sides, keeping your arms straight and your palms facing down.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (4)</name>
+        <message>
+            <source>Lift your arms up until they are at shoulder height, or slightly above.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Lift your arms up until they are at shoulder height, or slightly above.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (5)</name>
+        <message>
+            <source>Hold the position for a second, then slowly lower your arms back to your sides.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hold the position for a second, then slowly lower your arms back to your sides.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (6)</name>
+        <message>
+            <source>You can do this exercise with or without light dumbells depending on how strong you feel.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You can do this exercise with or without light dumbells depending on how strong you feel.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Lat_Raises2/Animated Say (7)</name>
+        <message>
+            <source>I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am going to explain how to do some arm raises. Listen to these instructions and then follow my lead.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well Done! 5 squats complete!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well Done! 5 squats complete!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ok then, keep in time with me.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ok then, keep in time with me.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stand width your feet shoulder-witdth apart</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stand width your feet shoulder-witdth apart</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Engage your core and keep your chest up</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Engage your core and keep your chest up</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lower your hips by bending your knees and pushing your hips back, as if you are sitting on a chair.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lower your hips by bending your knees and pushing your hips back, as if you are sitting on a chair.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Keep your knees in line with your toes and lower yourself until your thighs are parallel to the ground.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Keep your knees in line with your toes and lower yourself until your thighs are parallel to the ground.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Push through your heels and straighten your legs to return to standing position.
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Push through your heels and straighten your legs to return to standing position.
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Squats/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am going to explain how to do some squats. Listen to these instructions and then follow my lead.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am going to explain how to do some squats. Listen to these instructions and then follow my lead.</translation>
+        </message>
+    </context>
+</TS>
